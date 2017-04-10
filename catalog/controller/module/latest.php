b797eb952cff8c3f2be734dbfc4cc6dcf4fdac55
monkdaf <monkdaf@gmail.com>
2017-04-11 02:08:43 +0300

    data for attribute of product in latest now is work

--- catalog/controller/module/latest.php
+++ catalog/controller/module/latest.php
@@ -57,11 +57,12 @@ class ControllerModuleLatest extends Controller {
 				} else {
 					$rating = false;
 				}
-
+$data['attribute_groups'] = $this->model_catalog_product->getProductAttributes($this->request->get['product_id']);
 				$data['products'][] = array(
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
+					'attribute_groups' => $this->model_catalog_product->getProductAttributes($result['product_id']),
 					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
